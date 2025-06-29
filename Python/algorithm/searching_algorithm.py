8f331552967c650f92e482208d5c4e833ada0758
Ong Zi Xuan <ongzx37@gmail.com>
2025-06-29 16:13:06 +0800

    update binary search test

--- Python/algorithm/searching_algorithm.py
+++ Python/algorithm/searching_algorithm.py
@@ -58,40 +58,40 @@ class BinarySearchStep:
 
 
 def binary_search_test(arr):
+    from datetime import datetime
     import random, time
 
     bs = BinarySearch()
 
-    n = len(arr) - 1
+    size = len(arr)
+    n = size - 1
 
     # run 100 times to allocate memory first, so that later will be consistent
     for _ in range(100):
         target = random.choice(arr)
         bs.binarySearch(arr, 0, n, target)
 
-    # Best Case
-    mid = (0 + n) // 2
-    target = arr[mid]
-    start_time = time.perf_counter()
-    bs.binarySearch(arr, 0, n, target)
-    end_time = time.perf_counter()
-    best_case_time = (end_time - start_time) * 1000
-
-    # Average Case
     total_time = 0
-    for _ in range(100):
-        target = random.choice(arr)
+    lowest_time = float('inf')
+    highest_time = -1
+
+    print("Start time:", datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
+
+    for target in arr:
         start_time = time.perf_counter()
         bs.binarySearch(arr, 0, n, target)
         end_time = time.perf_counter()
-        total_time += (end_time - start_time) * 1000
-    average_case_time = total_time / 100
+        execution_time = ((end_time - start_time) * 1000)
+        if execution_time < lowest_time:
+            lowest_time = execution_time
+        if execution_time > highest_time:
+            highest_time = execution_time
+        total_time += execution_time
 
-    # Worst Case
-    target = max(arr) + 10
-    start_time = time.perf_counter()
-    bs.binarySearch(arr, 0, n, target)
-    end_time = time.perf_counter()
-    worst_case_time = (end_time - start_time) * 1000
+    print("End time  :", datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
+
+    best_case_time = lowest_time
+    worst_case_time = highest_time
+    average_case_time = total_time / size
 
     return [best_case_time, average_case_time, worst_case_time]
